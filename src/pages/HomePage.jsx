b1490c20644e06163f2f29cc96987954e4d0f45d
aarya shah <aarya.shah@mail.utoronto.ca>
2025-10-25 13:04:12 -0400

    removing navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from 'react'
-import Navbar from '../components/Navbar'
 
 function HomePage() {
     const [uploadedFile, setUploadedFile] = useState(null)
